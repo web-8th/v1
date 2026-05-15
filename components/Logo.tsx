ab863f41d291db5976f90ac54134c3537bc3ad24
rinmeng-gitbash <phurinmeng@gmail.com>
2026-05-15 05:58:16 -0700

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -2,7 +2,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '@/components/ui';
 import { cn } from '@/lib/utils';
-import { ChevronRight, Infinity } from 'lucide-react';
 
 interface LogoProps {
   className?: string;
@@ -15,13 +14,7 @@ export function Logo({ className, iconSize = 20, showText = true, onClick }: Log
   return (
     <div className='flex items-center gap-3'>
       <Button variant='link' className={cn('p-0 m-0', className)}>
-        <Link
-          href={
-            typeof window !== 'undefined' && window.location.hostname === 'localhost'
-              ? '/'
-              : 'https://web8th.com'
-          }
-        >
+        <Link href='/'>
           <Image
             className='not-dark:invert-100'
             src='/icons/8th_svg.svg'
